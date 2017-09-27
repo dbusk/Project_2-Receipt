3d1527b65107fc7e1e5c4383e09e0a336f4b29fb
dbusk <writelani@gmail.com>
2017-09-27 14:10:42 -0400

    Initial commit

---
+++ src/Receipt.java
@@ -0,0 +1,23 @@
+public class Receipt {
+
+    + - - - - - - - - - - - - - - - - - - - - - +
+    |                                           |
+    |          Java Bank ATM Receipt            |
+    |     Monday, September 18th, 2017          |
+    |           ATM Location: #24               |
+    |                                           |
+    |     Account Number:            90210      |
+    |     Customer:       Delani D. Buskey      |
+    |     Transaction Type:     Withdrawal      |
+    |     Transaction Amount:     $1000.64      |
+    |     Account Balance:        A lot         |
+    |                                           |
+    |                                           |
+    |                                           |
+    |       Thank you for banking with us!      |
+    |             Have a great day!             |
+    |                                           |
+    |                                           |
+    |                                           |
+    + - - - - - - - - - - - - - - - - - - - - - +
+}|
